e1c366500a05ea9c6a2c171f94c19513d073e128
umaa64 <uma.kandiboti@gmail.com>
2018-04-23 12:05:10 +0530

    Created Redshift file

---
+++ assets/web/redshift_analysis.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+
+import multiprocessing
+from configparser import ConfigParser
+
+import functools
+
+import logging
+
+import sys
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    jsonify,
+    abort
+)
+from flask import redirect
+from flask import url_for
+
+from analysis.transformations import *
+
+test = Flask(
+    __name__,
+    template_folder=os.path.join(PROJECT_DIR, 'web/templates'),
+    static_folder=os.path.join(PROJECT_DIR, 'web/static')
+)
+
+
+def parse_command_line_args():
+    parser = argparse.ArgumentParser(description='Quick start App')
+    parser.add_argument('--config', required=True, help='Configuration')
+    return parser.parse_args()
+
+def read_config(config_path):
+    parser = ConfigParser()
+    parser.read(config_path)
+    config = {}
+    for section in parser.sections():
+        for (config_key, config_value) in parser.items(section):
+            config[config_key] = config_value
+    return config
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+args = parse_command_line_args()
+config = read_config(args.config)
+test.secret_key = os.urandom(47)
+test.config.update(config)
+
+print("123")
+run_redshift_analysis(config)
